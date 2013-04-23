28474b5d54b8fd353a60c3cf66d7df959f986500
Phuong Nguyen <trangphuong618@gmail.com>
2013-04-23 17:06:23 +0700

    first

---
+++ projectk/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using projectk.Models;
+
+namespace projectk
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "273706276099741",
+                appSecret: "76f24899267f81e17c56266e69bb8ac0");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
---
+++ projectk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projectk.MvcApplication" Language="C#" %>
