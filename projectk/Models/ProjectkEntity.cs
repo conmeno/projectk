fcd7498a8013154adbdffd5d4512516c202eb0f5
Phuong Nguyen <trangphuong618@gmail.com>
2013-05-14 16:43:23 +0700

    r

---
+++ projectk/Models/ProjectkEntity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace projectk.Models
+{
+    public class ProjectkEntity:DbContext
+    {
+        
+            public ProjectkEntity()
+                : base("DefaultConnection")
+            {
+            }
+
+           // public DbSet<UserProfile> UserProfiles { get; set; }
+            public DbSet<Test1> test1s { get; set; }
+            public DbSet<Test2> test2s { get; set; }
+
+            public DbSet<UserProfile> UserProfiles { get; set; }
+         
+    }
+    public class Test1
+    {
+        public int ID { get; set; }
+        public string value { get; set; }
+    }
+    public class Test2
+    {
+        public int ID { get; set; }
+        public string value { get; set; }
+    }
+
+
+
+
+    
+
+    [Table("UserProfile")]
+    public class UserProfile
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+        public int TotalLike { get; set; }
+
+    }
+
+}
