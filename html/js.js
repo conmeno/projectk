b25e0f6120888d5746ca31c9b503be8746be3f37
conmeno <conmeno@outlook.com>
2013-05-19 00:01:28 +0700

    template

--- html/js.js
+++ html/js.js
@@ -1,11 +1,24 @@
 $(window).scroll(function () {
-    var header = $('header');
+    var header = $('.header');
     if ($(window).scrollTop() >= 38 & !header.hasClass('small')) {
         header.addClass('small');
+        $('#steps-home').css("top", '65px');
     }
     if ($(window).scrollTop() < 38 & header.hasClass('small')) {
         header.removeClass('small');
+        $('#steps-home').css("top", '100px');
     }
 
    
+   
+});
+$(document).ready(function () {
+    $('.panel,#steps-home').hover(function () {
+        var offset = $('.panel').position();
+        $('#steps-home').show();
+        //$('#steps-home').css("top",(offset.top + $('.panel').height()) + 'px');
+       
+    }, function () {
+        $('#steps-home').hide();
+    });
 });
