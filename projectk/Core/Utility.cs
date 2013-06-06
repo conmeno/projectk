51e36784ff9307cf516241ab8ab502d8324ce48e
conmeno <conmeno@outlook.com>
2013-06-06 23:05:00 +0700

    aa

---
+++ projectk/Core/Utility.cs
@@ -0,0 +1,34 @@
+﻿using projectk.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Projectk
+{
+    public class Utility
+    {
+       
+       
+        public static string GetUsername(Stream input)
+        {
+            return "";
+        }
+        public static int GetUserIDByUsername(string Username)
+        {
+            int rs=0;
+            ProjectkContext db = new ProjectkContext();
+            List<UserProfile> listu = db.UserProfiles.Where(a => a.UserName ==HttpContext.Current.User.Identity.Name).ToList();
+
+
+        
+            if (listu.Count > 0)
+            {
+                rs = listu[0].UserId;
+            }
+            return rs;
+        }
+
+    }
+}
