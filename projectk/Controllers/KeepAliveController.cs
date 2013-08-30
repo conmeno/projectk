c9b0eb33cdb77f5e0603fc7dc5ccce9cb0a53e8d
Phuong Nguyen <trangphuong618@gmail.com>
2013-08-30 16:48:03 +0700

    keep alive

---
+++ projectk/Controllers/KeepAliveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projectk.Controllers
+{
+    public class KeepAliveController : Controller
+    {
+        //
+        // GET: /KeepAlive/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string load()
+        {
+            return DateTime.Now.ToString();
+        }
+
+    }
+}
